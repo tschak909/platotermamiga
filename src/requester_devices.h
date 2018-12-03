8cca933be07d790fd1f0a5dbe81e89381a4b6f03
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2018-12-03 15:40:20 -0600

    add header.

---
+++ src/requester_devices.h
@@ -0,0 +1,8 @@
+/**
+ * PLATOTermAmiga - A PLATO Terminal for the Commodore Amiga
+ * Based on Steve Peltz's PAD
+ * 
+ * Author: Thomas Cherryhomes <thom.cherryhomes at gmail dot com>
+ *
+ * requester_devices.h - Device file requester
+ */
